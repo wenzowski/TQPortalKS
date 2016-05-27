59f27629968798f4ff34a94604cf8a0c279dfe52
Jack Park <jackpark@gmail.com>
2016-05-26 18:41:20 -0700

    first git

---
+++ apps/models/rpg_model.js
@@ -0,0 +1,12 @@
+/**
+ * Created by park on 5/26/2016.
+ */
+var Constants = require("../constants");
+
+var RpgModel =  module.exports = function(environment) {
+    var self = this,
+        topicDriver = environment.getTopicDriver(),
+        CommonModel = environment.getCommonModel();
+    console.log("RPGModel");
+
+};
